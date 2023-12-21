2a07a8e051853fcab806fd7892f3a09ca9d63d09
Purushottam Kumar <purushottam.kumar@krishitantra.com>
2023-12-21 18:44:26 +0530

    working one

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,22 @@
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 
+import org.openqa.selenium.Point;
+import org.openqa.selenium.interactions.Pause;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 public class Main {
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
@@ -25,6 +32,7 @@ public class Main {
 
 
 
+        /// login done
         var el1 = driver.findElement(AppiumBy.accessibilityId("Show drop-down menu"));
         el1.click();
         var el2 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/et_email"));
@@ -36,76 +44,75 @@ public class Main {
         var el5 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/btnLogin"));
         el5.click();
 
-        /*
 
+        TimeUnit.SECONDS.sleep(10);
+
+        // register farmer
         var el7 = driver.findElement(AppiumBy.xpath("//android.widget.RelativeLayout[@resource-id=\"com.kt_goi_shc:id/rl_farmerRegistration\"]/android.widget.LinearLayout"));
         el7.click();
         var el8 = driver.findElement(AppiumBy.xpath("(//android.widget.ImageButton[@content-desc=\"Show drop-down menu\"])[1]"));
         el8.click();
         var el9 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/tv_blockName"));
         el9.click();
+        TimeUnit.SECONDS.sleep(10);
         var el10 = driver.findElement(AppiumBy.xpath("(//android.widget.ImageButton[@content-desc=\"Show drop-down menu\"])[2]"));
         el10.click();
         var el11 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/btn_submit"));
         el11.click();
         var el12 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/btn_submit"));
         el12.click();
+
+        TimeUnit.SECONDS.sleep(10);
+
+
+        // register farmer click
         var el13 = driver.findElement(AppiumBy.xpath("//android.widget.RelativeLayout[@resource-id=\"com.kt_goi_shc:id/rl_farmerRegistration\"]/android.widget.LinearLayout"));
         el13.click();
-        var el14 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/edit_txtName"));
-        el14.sendKeys("Purushottam Kumar");
-        var el15 = driver.findElement(AppiumBy.xpath("(//android.widget.ImageButton[@content-desc=\"Show drop-down menu\"])[1]"));
-        el15.click();
-        var el16 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/edit_txtPhone"));
-        el16.click();
-        var el17 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/edit_txtPhone"));
-        el17.sendKeys("8130461500");
-        var el18 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/tv_villageName"));
-        el18.click();
-        var el19 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/textinput_placeholder"));
-        el19.click();
-        var el20 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/et_address"));
-        el20.sendKeys("Mangalore");
-        var el21 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/submit"));
-        el21.click();
-        var el22 = driver.findElement(AppiumBy.xpath("(//android.widget.TextView[@text=\"Sample Collection\"])[2]"));
-        el22.click();
-        el22.click();
-        var el23 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/mtv_userName"));
+
+
+
+        var el22 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/edit_txtName"));
+        el22.sendKeys("Purushottam Kumar");
+
+        var el23 = driver.findElement(AppiumBy.xpath("(//android.widget.ImageButton[@content-desc=\"Show drop-down menu\"])[1]"));
         el23.click();
-        el23.sendKeys("8130461500");
-        var el24 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/btn_submit"));
+        var el24 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/rlClickConsumer"));
         el24.click();
-        var el25 = driver.findElement(AppiumBy.xpath("//androidx.recyclerview.widget.RecyclerView[@resource-id=\"com.kt_goi_shc:id/farmersRecyclerView\"]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[3]"));
-        el25.click();
-        var el26 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/img_location1"));
+
+        var el25= driver.findElement(AppiumBy.id("com.kt_goi_shc:id/edit_txtPhone"));
+        el25.sendKeys("8130461500");
+
+        //swipe
+        final var finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        var start = new Point(1034, 1805);
+        var end = new Point (1030, 822);
+        var swipe = new Sequence(finger, 1);
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(0),
+                PointerInput.Origin.viewport(), start.getX(), start.getY()));
+        swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(1000),
+                PointerInput.Origin.viewport(), end.getX(), end.getY()));
+        swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+        driver.perform(Arrays.asList(swipe));
+
+
+
+        var el26 = driver.findElement(AppiumBy.accessibilityId("Show drop-down menu"));
         el26.click();
-        var el27 = driver.findElement(AppiumBy.xpath("(//android.widget.ImageButton[@content-desc=\"Show drop-down menu\"])[2]"));
+
+        var el27 = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Farmer Registration\"]"));
         el27.click();
-        var el28 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/tv_address1"));
-        el28.click();
-        var el29 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/et_surveyNumber1"));
-        el29.sendKeys("223344");
-        var el30 = driver.findElement(AppiumBy.xpath("(//android.widget.ImageButton[@content-desc=\"Show drop-down menu\"])[3]"));
-        el30.click();
-        var el31 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/et_mandal"));
-        el31.click();
-        var el32 = driver.findElement(AppiumBy.xpath("(//android.widget.ImageButton[@content-desc=\"Show drop-down menu\"])[3]"));
-        el32.click();
-        var el33 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/et_areaSize1"));
-        el33.click();
-        var el34 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/et_areaSize1"));
-        el34.click();
-        el34.sendKeys("222");
-        var el35 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/img_upload"));
-        el35.click();
-        var el36 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/btn_collectSoilSample"));
-        el36.click();
-        var el37 = driver.findElement(AppiumBy.id("android:id/button2"));
-        el37.click();
+        var el28 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/et_address"));
+        el28.sendKeys("Mangalore");
 
-        */
+        var el29 = driver.findElement(AppiumBy.id("com.kt_goi_shc:id/submit"));
+        el29.click();
 
+
+    }
+
+    public void registerFarmer()
+    {
 
     }
 }
